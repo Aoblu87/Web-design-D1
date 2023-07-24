c63b15e542aaa7d0ba3f1efe687e9fc32515ead7
Stefania <stefania.struzzi@gmail.com>
2023-07-24 22:43:59 +0200

    finiti es js, con problema su querySelectorAll

--- script.js
+++ script.js
@@ -12,19 +12,18 @@
 
 
 
-// window.onload= function(){
+window.onload= function(){
 
-//     const twitter= document.querySelector('.p-4:nth-child(3) li:nth-child(2)')
-
-
-
-
-// twitter.style.visibility= "hidden"
+    const twitter= document.querySelector('.p-4:nth-child(3) li:nth-child(2)')
 
 
 
 
-// }
+twitter.style.visibility= "hidden"
+
+
+
+
 
 
 
@@ -32,18 +31,47 @@
 
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
-//  ::ACTIVE
-const postContainer= document.querySelectorAll('.col')
+const postContainer1= document.querySelector('.col')
+const postContainer2= document.querySelector('div:nth-child(2) > div > div.col')
 const bigPostContainer= document.querySelector('.col-md-6')
 
-const aPost= document.querySelectorAll('.col a')
+const aPost= document.querySelectorAll('.col a')  //PERCHE SE USO QUERY SELECTONALL NON FUNZIONA!!!!
+const post= document.querySelector('.stretched-link')
+const post2= document.querySelector('div:nth-child(2) > div > div.col > a')
 const aBigPost=document.querySelector('.lead a')
 
 
+aBigPost.addEventListener('click', function (event){
+    
+    bigPostContainer.remove()
+    
+})
+
+post.addEventListener('click', function(event){
+    
+    
+    postContainer1.remove()
+})
+
+post2.addEventListener('click', function(){
+    
+    postContainer2.remove()
+    
+})
 
 
+
+
+}
 
 
 // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
 
+const author= document.querySelector('.blog-post-meta a') //CONTINUA A NON FUNZIONARE SE USO QUERYSELECTORALL!!!!!!
+
+author.onmouseover = function(){
+
+alert(author.innerText)
+
+};
 
