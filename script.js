492b7a80e70a8edd45276ca55ed38ec4ef54bd14
Stefania <stefania.struzzi@gmail.com>
2023-07-24 21:21:07 +0200

    inizio esercizi js

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
-         // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
-         // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
- 
 
-         
+
+// aggiungere attributo con valore true
+
+/* Selettore per il primo esercizio di JS */
+// .p-4:nth-child(3) li:nth-child(2){
+//     background-color: green;
+//     }
+
+
+
+
+
+// window.onload= function(){
+
+//     const twitter= document.querySelector('.p-4:nth-child(3) li:nth-child(2)')
+
+
+
+
+// twitter.style.visibility= "hidden"
+
+
+
+
+// }
+
+
+
+
+
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+//  ::ACTIVE
+const postContainer= document.querySelectorAll('.col')
+const bigPostContainer= document.querySelector('.col-md-6')
+
+const aPost= document.querySelectorAll('.col a')
+const aBigPost=document.querySelector('.lead a')
+
+
+
+
+
+
+// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+
+
